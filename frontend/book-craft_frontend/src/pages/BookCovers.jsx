5afb03d23e1e137be55227fc4f4067efe1cde41f
Favour Michael <michaelfavour418@gmail.com>
2024-06-06 09:34:42 +0100

    linking login and registration pages to backend

--- frontend/book-craft_frontend/src/pages/BookCovers.jsx
+++ frontend/book-craft_frontend/src/pages/BookCovers.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+// import Hero from "../components/Hero";
 
 const BookCovers = () => {
-  return (
-    <div className='w-full h-screen'>
-      <div className='border-2 border-black'>
-        <div>
+    const [books, setBooks] = useState([]);
 
-        </div>
-      </div>
-    </div>
-  )
+    useEffect(() => {
+        const fetchBooks = async () => {
+            try {
+                const response = await axios.get('https://freetestapi.com/api/v1/books?limit=10');
+                setBooks(response.data);
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+            }
+        };
+
+        fetchBooks();
+    }, []);
+
+    return (
+        <>
+            {/* <Hero /> */}
+            {Array.isArray(books) && books.map((book, index) => (
+              <div className='flex flex-col col-span-3'>
+                <div key={index} className='p-4 m-10 shadow-lg hover:scale-105 shadow-[#00ACD0]'>
+                    <h2 className='text-3xl'>{book.title}</h2>
+                    <p className='text-red-300'>{book.author}</p>
+                </div>
+              </div>
+            ))}
+            <p className='p-5'>test</p>
+        </>
+    )
 }
 
-export default BookCovers
+export default BookCovers;
--- frontend/book-craft_frontend/src/pages/Login.jsx
+++ frontend/book-craft_frontend/src/pages/Login.jsx
@@ -1,39 +1,84 @@
-import { Link } from 'react-router-dom'
-
-import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    password: ""
+  });
+  const { email, password } = inputValue;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+  const handleError = (error) => toast.error(error, {position: "top-center"});
+  const handleSuccess = (msg) => toast.success(msg, {position: "top-center"});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("http://localhost:5000/login",
+      {
+        ...inputValue,
+      },
+      { withCredentials: true }
+      );
+      const { success, message } = data;
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate('/');
+        }, 1000);
+      } else {
+        handleError(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setInputValue({
+      ...inputValue,
+      email: "",
+      password: "",
+    });
+  };
   return (
     <div className='h-screen w-full p-auto flex justify-center items-center flex-col'>
       <h1 className='text-3xl font-bold text-center text-[#004aad] mb-3'>Log In</h1>
       <div className='w-3/4 h-3/4 border-2 border-[#00ACD0] shadow-2xl shadow-[#00ACD0] bg-white flex justify-center items-center flex-col'>
-      <form className="space-y-4 md:space-y-6" action="#">
+      <form className="space-y-4 md:space-y-6 w-1/2 h-1/2" onSubmit={handleSubmit} action="#">
         <div>
-              <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" required="" />
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+              <input onChange={handleChange} value={email} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" required={true} />
           </div>
           <div>
-              <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-              <input type="password" name="password" id="password" placeholder="•••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+              <input onChange={handleChange} value={password} type="password" name="password" id="password" placeholder="•••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required={true} />
           </div>
           <div className="flex items-center justify-between">
               <div className="flex items-start">
                   <div className="flex items-center h-5">
-                    <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required="" />
+                    <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required={true} />
                   </div>
                   <div className="ml-2 text-sm">
-                    <label for="remember" className="text-gray-500">Remember me</label>
+                    <label htmlFor="remember" className="text-gray-500">Remember me</label>
                   </div>
               </div>
               <Link className="text-sm font-medium text-primary-600 hover:underline m-4">Forgot password?</Link>
           </div>
           <button type="submit" className="w-full text-black bg-[#004aad] hover:bg-[#477ec6] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
           <p className="text-sm font-light text-gray-500">
-              Don't have an account yet? <Link className="font-medium text-primary-600 hover:underline">Sign up</Link>
+              Don't have an account yet? <Link to={"/register"} className="font-medium text-primary-600 hover:underline">Register</Link>
           </p>
         </form>
 
       </div>
+      <ToastContainer />
     </div>
   )
 }
